4f47843db4bf35baa05aff01e27bd867b86d117f
RabeyaEty01 <etyrabeya01@gmail.com>
2021-08-14 20:14:47 +0600

    addEventListener and onclick example

--- js/events.js
+++ js/events.js
@@ -13,3 +13,35 @@ Makeblue.onclick= makeBlue;
 function makeBlue(){
     document.body.style.backgroundColor='blue';
 }
+
+
+//handle event using anonymous function
+
+const greenButton = document.getElementById('make-green-button');
+//anonymous function
+greenButton.onclick = function () {
+    document.body.style.backgroundColor = 'green';
+}
+
+//handle event using add eventlistener function
+
+const goldenButton= document.getElementById('make-goldenrod');
+goldenButton.addEventListener('click',makeGoldenRod );
+
+function makeGoldenRod(){
+    document.body.style.backgroundColor='goldenrod';
+}
+
+//addEventListener
+
+const hotPinkButton =document.getElementById('make-hotpink');
+
+hotPinkButton.addEventListener('click', function(){
+    document.body.style.backgroundColor='hotpink';
+})
+
+
+//direct shortcut 
+document.getElementById('make-light-blue').addEventListener('click', function(){
+    document.body.style.backgroundColor='lightblue';
+})
